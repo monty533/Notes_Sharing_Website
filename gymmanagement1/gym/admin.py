c1e4dd987cd9f1e8d9be63036098fe1ddab76c26
Nitin Kumar <70315471+monty533@users.noreply.github.com>
2022-08-18 18:58:46 +0530

    Delete gymmanagement1 directory

--- gymmanagement1/gym/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from gym.models import Enquiry,Equipment,Plan,Member
-
-# Register your models here.
-admin.site.register(Enquiry)
-admin.site.register(Equipment)
-admin.site.register(Plan)
-admin.site.register(Member)
