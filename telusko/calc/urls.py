49257e1a0908bd67a0652df4319ce88acf64b81e
Nitin Kumar <70315471+monty533@users.noreply.github.com>
2022-08-18 18:59:12 +0530

    Delete telusko directory

--- telusko/calc/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='Home'),
-    path('add', views.add, name='Add'),
-]
